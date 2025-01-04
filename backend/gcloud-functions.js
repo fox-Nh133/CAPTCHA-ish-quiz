7e98465038add03af9f9586024d291ca9d49bb8c
Masaya <masaya@matsugasaki.dev>
2025-01-04 23:17:06 +0900

    fix CORS policy and typo

--- backend/gcloud-functions.js
+++ backend/gcloud-functions.js
@@ -3,16 +3,16 @@ const functions = require('@google-cloud/functions-framework');
 functions.http('validateAnswer', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Methods', 'POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
         res.status(204).send('');
         return;
     }
 
-    const question = req.get('Question');
     let predefineSet;
 
-    prefineSet = new Set(['image1', 'image2', 'image3']);
+    predefineSet = new Set(['image1', 'image2', 'image3']);
     const receivedSet = new Set (req.body.items);
 
     let isValid = true;
